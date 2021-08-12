c83a943fb537bbb28f328e2379d0332f5b546da2
aerdon085 <adrianluzon36@gmail.com>
2021-08-12 08:26:38 +0800

    Added notes

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
 // document.getElementById("count-el").innerText = 5
 
-
-// let firstBatch = 5
-// let secondBatch = 7
-
-// let count = firstBatch + secondBatch
-// console.log(count)
-
-// count = count + firstBatch
-// console.log(count)
-
-// count = count + firstBatch
-// console.log(count)
+// function increment() {
+//     console.log("Nice")
+// }
 
 
-// let score = 0
-// let scoreMultiplier = score * 0.001
-// console.log(scoreMultiplier)
-
-// let scoreInitialMultiplier = 1 + scoreMultiplier
-// console.log(scoreInitialMultiplier)
-
-// totalScore = score * scoreInitialMultiplier
-// console.log(totalScore)
---
+++ others/note.js
@@ -0,0 +1,89 @@
+//SECTION: Variables
+
+
+// let firstBatch = 5
+// let secondBatch = 7
+
+// let count = firstBatch + secondBatch
+// console.log(count)
+
+// count = count + firstBatch
+// console.log(count)
+
+// count = count + firstBatch
+// console.log(count)
+
+
+//SECTION: Basic Calc with Variables
+
+
+// let score = 0
+// let scoreMultiplier = score * 0.001
+// console.log(scoreMultiplier)
+
+// let scoreInitialMultiplier = 1 + scoreMultiplier
+// console.log(scoreInitialMultiplier)
+
+// totalScore = score * scoreInitialMultiplier
+// console.log(totalScore)
+
+
+// SECTION: Function
+
+
+// function nice() {
+//     console.log(42)
+// }
+
+// nice()
+
+
+// SECTION: Calc Function
+
+
+// let lap1 = 34
+// let lap2 = 33
+// let lap3 = 36
+
+// function total() {
+//     let lapInitial = lap1 + lap2 + lap3
+
+//     console.log(lapInitial)
+// }
+
+// total()
+
+
+// SECTION: Increment that results the sum per increment
+
+
+// let lapsCompleted = 0
+
+// function lapIncrement() {
+//     lapsCompleted = lapsCompleted + 1
+//     console.log(lapsCompleted)
+// }
+
+// lapIncrement()
+// lapIncrement()
+// lapIncrement()
+
+// // === 1, 2, 3
+
+
+// SECTION: Increment that results the sum of all increments combined
+
+
+// let lapsCompleted = 0
+
+// function lapIncrement() {
+//     lapsCompleted = lapsCompleted + 1
+// }
+
+// lapIncrement()
+// lapIncrement()
+// lapIncrement()
+
+// console.log(lapsCompleted)
+
+// // === 3
