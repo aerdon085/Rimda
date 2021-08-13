95b3ef6837cda27f379bc88023c1ba783a24f036
aerdon085 <adrianluzon36@gmail.com>
2021-08-13 19:23:26 +0800

    Finished basic counter with reset button

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 // document.getElementById("count-el").innerText = 5
 
-// function increment() {
-//     console.log("Nice")
-// }
+let countEl = document.getElementById("count-el")
 
+// console.log(countEl)
 
+let count = 0
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+    console.log(count)
+}
+
+function reset() {
+    count = 0
+    countEl.innerText = count
+    console.log(count)
+}
