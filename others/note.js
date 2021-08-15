3c4281418dcf74ebc145020eac1b1668b6b6ddfe
aerdon085 <adrianluzon36@gmail.com>
2021-08-15 14:33:26 +0800

    Strings vs numbers

--- others/note.js
+++ others/note.js
@@ -1,72 +1,72 @@
 //SECTION: Variables
 
 
-// let firstBatch = 5
-// let secondBatch = 7
+let firstBatch = 5
+let secondBatch = 7
 
-// let count = firstBatch + secondBatch
-// console.log(count)
+let count = firstBatch + secondBatch
+console.log(count)
 
-// count = count + firstBatch
-// console.log(count)
+count = count + firstBatch
+console.log(count)
 
-// count = count + firstBatch
-// console.log(count)
+count = count + firstBatch
+console.log(count)
 
 
 //SECTION: Basic Calc with Variables
 
 
-// let score = 0
-// let scoreMultiplier = score * 0.001
-// console.log(scoreMultiplier)
+let score = 0
+let scoreMultiplier = score * 0.001
+console.log(scoreMultiplier)
 
-// let scoreInitialMultiplier = 1 + scoreMultiplier
-// console.log(scoreInitialMultiplier)
+let scoreInitialMultiplier = 1 + scoreMultiplier
+console.log(scoreInitialMultiplier)
 
-// totalScore = score * scoreInitialMultiplier
-// console.log(totalScore)
+totalScore = score * scoreInitialMultiplier
+console.log(totalScore)
 
 
 // SECTION: Function
 
 
-// function nice() {
-//     console.log(42)
-// }
+function nice() {
+    console.log(42)
+}
 
-// nice()
+nice()
 
 
 // SECTION: Calc Function
 
 
-// let lap1 = 34
-// let lap2 = 33
-// let lap3 = 36
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
 
-// function total() {
-//     let lapInitial = lap1 + lap2 + lap3
+function total() {
+    let lapInitial = lap1 + lap2 + lap3
 
-//     console.log(lapInitial)
-// }
+    console.log(lapInitial)
+}
 
-// total()
+total()
 
 
 // SECTION: Increment that results the sum per increment
 
 
-// let lapsCompleted = 0
+let lapsCompleted = 0
 
-// function lapIncrement() {
-//     lapsCompleted = lapsCompleted + 1
-//     console.log(lapsCompleted)
-// }
+function lapIncrement() {
+    lapsCompleted = lapsCompleted + 1
+    console.log(lapsCompleted)
+}
 
-// lapIncrement()
-// lapIncrement()
-// lapIncrement()
+lapIncrement()
+lapIncrement()
+lapIncrement()
 
 // // === 1, 2, 3
 
@@ -74,16 +74,81 @@
 // SECTION: Increment that results the sum of all increments combined
 
 
-// let lapsCompleted = 0
+let lapsCompleted = 0
 
-// function lapIncrement() {
-//     lapsCompleted = lapsCompleted + 1
-// }
+function lapIncrement() {
+    lapsCompleted = lapsCompleted + 1
+}
 
-// lapIncrement()
-// lapIncrement()
-// lapIncrement()
+lapIncrement()
+lapIncrement()
+lapIncrement()
 
-// console.log(lapsCompleted)
+console.log(lapsCompleted)
 
 // // === 3
+
+
+// SECTION: MOD
+
+
+// document.getElementById("count-el").innerText = 5
+
+let countEl = document.getElementById("count-el")
+
+let lapEl = document.getElementById("lap-el")
+
+// console.log(countEl)
+
+let count = 0
+
+let storeLap = 0
+
+function increment() {
+    // add and display count
+    count = count + 1
+    countEl.innerText = count
+    console.log(count)
+}
+
+function lap() {
+    // move count into lap and display
+    storeLap = storeLap + count
+    lapEl.innerText = storeLap
+    // then delete current data of count and display for full effect of the technical term used: "move"
+    count = 0
+    countEl.innerText = count
+    console.log(count)
+}
+
+function reset() {
+    // reset count, if any, then display
+    count = 0
+    countEl.innerText = count
+    // reset storeLap, if any, then display
+    storeLap = 0
+    lapEl.innerText = storeLap
+    console.log(count)
+}
+
+
+// SECTION: Strings
+
+
+let notif = ", you have three new messages."
+
+let user = "Adrian"
+
+console.log(user + notif)
+
+
+// SECTION: Strings pt.2
+
+
+let username = "aerdon085"
+
+let message = "You have three new notifications"
+
+let messageToUser = message + ", " + username + "!"
+
+console.log(messageToUser)
