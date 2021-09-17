683ad41181e603c6d3f6232d5dad47d30e17ef35
aerdon085 <adrianluzon36@gmail.com>
2021-09-17 17:52:42 +0800

    Rock, paper, scissors
    Lacks player's option to decide what hand he'll play
    
    I also want to try: Points system

--- Projects/Test/test.js
+++ Projects/Test/test.js
@@ -1,14 +1,20 @@
 let pOneDis = document.querySelector("#pOne")
 let pTwoDis = document.querySelector("#pTwo")
+let greetingEl = document.querySelector("#greeting-el")
 
+let playerOne = 0
+let playerTwo = 0
 let handSign = ""
 
+// function that starts the game when pressed #start-btn
 function start() {
-    let playerOne = draw()
-    let playerTwo = draw()
+    playerOne = draw()
+    playerTwo = draw()
     // display to HTML
     pOneDis.textContent = "Player 1: " + playerOne
     pTwoDis.textContent = "Player 2: " + playerTwo
+    // display winner of game
+    checkWin()
 }
 
 function draw() {
@@ -25,4 +31,14 @@ function draw() {
     }
     console.log(handSign)
     return handSign
+}
+
+function checkWin() {
+    if (playerOne === "Rock" && playerTwo === "Scissors" || playerOne === "Paper" && playerTwo === "Rock" || playerOne === "Scissors" && playerTwo === "Paper") {
+        greetingEl.textContent = "Player 1 won!"
+    } else if (playerOne === "Rock" && playerTwo === "Rock" || playerOne === "Paper" && playerTwo === "Paper" || playerOne === "Scissors" && playerTwo === "Scissors") {
+        greetingEl.textContent = "It's a tie!"
+    } else {
+        greetingEl.textContent = "Player 2 won!"
+    }
 }
