9df1d71d7fbd016af92cf808df33e90ff9a3c3e2
aerdon085 <adrianluzon36@gmail.com>
2021-09-19 11:54:15 +0800

    build chrome extension
    test.js problem how to shift in for loop

--- Projects/Test/test.js
+++ Projects/Test/test.js
@@ -1,44 +1,16 @@
-let pOneDis = document.querySelector("#pOne")
-let pTwoDis = document.querySelector("#pTwo")
-let greetingEl = document.querySelector("#greeting-el")
-
-let playerOne = 0
-let playerTwo = 0
-let handSign = ""
-
-// function that starts the game when pressed #start-btn
-function start() {
-    playerOne = draw()
-    playerTwo = draw()
-    // display to HTML
-    pOneDis.textContent = "Player 1: " + playerOne
-    pTwoDis.textContent = "Player 2: " + playerTwo
-    // display winner of game
-    checkWin()
-}
-
-function draw() {
-    // represent hand signs with numbers
-    let drawValue = Math.floor(Math.random() * 3) + 1
-    console.log("Drawing... " + drawValue)
-    // translate number to hand sign
-    if (drawValue === 1) {
-        handSign = "Rock"
-    } else if (drawValue === 2) {
-        handSign = "Paper"
-    } else {
-        handSign = "Scissors"
-    }
-    console.log(handSign)
-    return handSign
-}
-
-function checkWin() {
-    if (playerOne === "Rock" && playerTwo === "Scissors" || playerOne === "Paper" && playerTwo === "Rock" || playerOne === "Scissors" && playerTwo === "Paper") {
-        greetingEl.textContent = "Player 1 won!"
-    } else if (playerOne === "Rock" && playerTwo === "Rock" || playerOne === "Paper" && playerTwo === "Paper" || playerOne === "Scissors" && playerTwo === "Scissors") {
-        greetingEl.textContent = "It's a tie!"
-    } else {
-        greetingEl.textContent = "Player 2 won!"
+let fruit = ["Apple", "Orange", "Orange", "Apple", "Orange", "Apple"]
+let appleShelf = document.querySelector("#apple-shelf")
+let orangeShelf = document.querySelector("#orange-shelf")
+function sort() {
+    for (let i = 0; i < fruit.length; i++) {
+        if (fruit[i] === "Apple") {
+            appleShelf.textContent += " " + fruit[i]
+        } else if (fruit[i] === "Orange") {
+            orangeShelf.textContent += " " + fruit[i]
+        }
+        console.log("Fruit shelved: " + fruit[i])
+        // fruit.shift()
     }
 }
+sort()
+console.log("Fruits left unshelved: " + fruit)
