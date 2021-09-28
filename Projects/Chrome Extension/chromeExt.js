4d313f9608e0998446b62c17b746807dfcee9603
aerdon085 <adrianluzon36@gmail.com>
2021-09-28 10:21:34 +0800

    New TicTacToe project
    temp.html problem how to shift in a for loop

--- Projects/Chrome Extension/chromeExt.js
+++ Projects/Chrome Extension/chromeExt.js
@@ -22,10 +22,8 @@ inputBtn.addEventListener("click", function() {
         // list current array items
         console.log("Input saved! Current items:")
         for (let i = 0; i < myLeads.length; i++) {
-            /*
-            console.log(myLeads[i])
-            ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-            */
+            // console.log(myLeads[i])
+            // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
             const li = document.createElement("li")
             li.textContent = myLeads[i]
             ulEl.append(li)
